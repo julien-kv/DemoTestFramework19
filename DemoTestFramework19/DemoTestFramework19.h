6054a1072dce7192e0f716f1afb0a9ac839bf02e
julienKeyvalue <julien@keyvalue.systems>
2023-05-19 07:09:58 +0530

    Initial Commit

---
+++ DemoTestFramework19/DemoTestFramework19.h
@@ -0,0 +1,18 @@
+//
+//  DemoTestFramework19.h
+//  DemoTestFramework19
+//
+//  Created by Julien on 19/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoTestFramework19.
+FOUNDATION_EXPORT double DemoTestFramework19VersionNumber;
+
+//! Project version string for DemoTestFramework19.
+FOUNDATION_EXPORT const unsigned char DemoTestFramework19VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoTestFramework19/PublicHeader.h>
+
+
